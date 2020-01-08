8eb09d874f11534ea982b893f9c3eff1ffb6d387
zack <zacharyzhangq1990@gmail.com>
2020-01-08 11:43:53 +0800

    project inti

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import './assets/styles/border.css';
+import './assets/styles/reset.css';
+import fastClick from 'fastclick';
 
-Vue.config.productionTip = false
+fastClick.attach(document.body);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
